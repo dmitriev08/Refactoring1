adcebe31533b27fea67be550fae61f2405430f42
dav <andrei-dmitriev@ya.ru>
2023-01-25 18:21:12 +0300

    change variable into method call.

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -17,21 +17,32 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         String result = "Выписка для " + getName() + "\n";
         for (Rental each: rentals) {
             double thisAmount = each.forAmount();
-
-            frequentRenterPoints += each.getFrequentPoints();
             result += "\t" + each.getCar().getTitle() + "\t" +
                     thisAmount + "\n";
-            totalAmount += thisAmount;
         } //while loop
-        result += "К оплате " + totalAmount + " руб\n";
+        result += "К оплате " + getTotalAmount() + " руб\n";
         result += "Вы заработали  " +
-                frequentRenterPoints + " бонусных баллов.";
+                getTotalFrequentPoints() + " бонусных баллов.";
         return result;
+    }
+
+    private int getTotalFrequentPoints() {
+        int ret = 0;
+        for (Rental each : rentals) {
+            ret += each.getFrequentPoints();
+        }
+        return ret;
+    }
+
+    private double getTotalAmount() {
+        double ret = 0;
+        for (Rental each : rentals) {
+            ret += each.forAmount();
+        }
+        return ret;
     }
 
 }
