a109f3ea78adc9cc4cc170027ce211ce647891f2
dav <andrei-dmitriev@ya.ru>
2023-01-25 18:45:08 +0300

    Add sport car into test.

--- src/main/java/AvisRentals.java
+++ src/main/java/AvisRentals.java
@@ -1,12 +1,15 @@
 public class AvisRentals {
     public static void main(String[] args) {
         Customer andrey = new Customer("Andrey Dmitriev");
-        Car renoDuster = new Car("Reno Duster", Car.REGULAR);
-        Car ladaGranta = new Car("Lada Granta", Car.SPORT);
+        Car renoDuster = new Car("WV Caddy", Car.MINI_VAN);
+        Car ladaGranta = new Car("Lada Granta", Car.REGULAR);
+        Car ferrari = new Car("Ferrari Car", Car.SPORT);
         Rental rent = new Rental(renoDuster, 5);
         Rental rent2 = new Rental(ladaGranta, 1);
+        Rental rent3 = new Rental(ferrari, 2);
         andrey.addRental(rent);
         andrey.addRental(rent2);
+        andrey.addRental(rent3);
         System.out.println(andrey.statement());
     }
 }
